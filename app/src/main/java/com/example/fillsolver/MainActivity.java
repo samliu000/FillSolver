4b8f6a349b5827a2ff683186495b75e413583516
samliu000 <samuel.liu1004@gmail.com>
2020-06-09 17:51:52 -0700

    added button drawable

--- app/src/main/java/com/example/fillsolver/MainActivity.java
+++ app/src/main/java/com/example/fillsolver/MainActivity.java
@@ -1,18 +1,16 @@
 package com.example.fillsolver;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.gridlayout.widget.GridLayout;
-
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Vector;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -116,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
                     cellButton.setBackgroundResource(android.R.drawable.btn_default);
                     cellButton.setText("");
                 }
-
+                ((Button)findViewById(R.id.setStart)).setBackgroundColor(Color.parseColor("#d2cced"));
                 solve = new FillSolver(8,6);
 
                 startRow = 0;
